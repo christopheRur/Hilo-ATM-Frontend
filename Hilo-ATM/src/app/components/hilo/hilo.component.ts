f1e8d435ddb46373218e31db6e4c76189d406b4b
Rurangwa <chrisruu@yahoo.com>
2024-01-19 16:48:28 -0500

    update component

---
+++ Hilo-ATM/src/app/components/hilo/hilo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hilo',
+  standalone: true,
+  imports: [],
+  templateUrl: './hilo.component.html',
+  styleUrl: './hilo.component.scss'
+})
+export class HiloComponent {
+
+}
