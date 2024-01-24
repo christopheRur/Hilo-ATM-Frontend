87a1056ee1cc1ce92c69443e91dad3e194b99ba8
Rurangwa <chrisruu@yahoo.com>
2024-01-24 12:10:23 -0500

    updates

--- Hilo-ATM/src/app/app.component.ts
+++ Hilo-ATM/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HiloComponent } from "./components/hilo/hilo.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, HiloComponent]
 })
 export class AppComponent {
   title = 'Hilo-ATM';
