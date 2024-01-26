00a7552abd9990fbc544dd2501e7bf3505d86074
Rurangwa <chrisruu@yahoo.com>
2024-01-26 13:48:45 -0500

    update

--- Hilo-ATM/src/app/app.module.ts
+++ Hilo-ATM/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatCardModule} from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HiloComponent } from './components/hilo/hilo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HiloComponent
   ],
   imports: [
     BrowserModule,
---
+++ Hilo-ATM/src/app/components/hilo/hilo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hilo',
+  templateUrl: './hilo.component.html',
+  styleUrl: './hilo.component.scss'
+})
+export class HiloComponent {
+
+}
