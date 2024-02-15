1258a09ad475974925388f065fed42f3a6c481ba
Rurangwa <chrisruu@yahoo.com>
2024-02-15 11:46:19 -0500

    update

--- Hilo-ATM/src/app/Services/hilo.service.ts
+++ Hilo-ATM/src/app/Services/hilo.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HiloService {
 
-  constructor() { }
+  private apiServiceUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
+  public retrieveCurrentBalance():void{}
+
+
 }
